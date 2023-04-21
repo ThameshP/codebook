7cf817a1913a9298992c875eb5da298c589b191e
ThameshP <72529324+ThameshP@users.noreply.github.com>
2023-04-20 20:23:50 -0400

    test

--- src/server/api/routers/subject.ts
+++ src/server/api/routers/subject.ts
@@ -16,6 +16,7 @@ export const subjectRouter = createTRPCRouter({
     .mutation(({ ctx, input }) => {
       return ctx.prisma.subject.create({
         data: {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
           title: input.title,
           userId: ctx.session.user.id,
         },
